36bb0c25932d46ff36bf9af1eec04f4e145d5e31
thomasgermain07 <thomasgermain07@hotmail.fr>
2022-07-08 11:56:06 +0200

    feat(client/next): installed sharp for better image performance, allowed aws image sources, create redirection from / to homepage

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -4,6 +4,25 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  experimental: {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "project0-images.s3.eu-west-3.amazonaws.com",
+        },
+      ],
+    },
+  },
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/homepage",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
