b7a45c5517546adfc2c1052be99e405d01e89e50
ThomasGermain-Macbook <thomasgermain07@hotmail.fr>
2022-07-14 11:43:05 +0200

    feat(client/homepage): now use a customHook to fetch data

--- packages/client/pages/homepage.tsx
+++ packages/client/pages/homepage.tsx
@@ -1,60 +1,43 @@
 import type { NextPage, GetStaticPropsContext } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { AppBarWrapper } from "../components/AppBarWrapper";
-import { BandImage } from "../components/BandImage";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { useTranslation } from "next-i18next";
-import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
 import { PageLayout } from "../styles/components/PageLayout";
-
-const CustomDivider = () => {
-  return (
-    <Divider
-      sx={{
-        width: "800px",
-        alignSelf: "center",
-        borderColor: "neutral.dark",
-      }}
-    />
-  );
-};
+import { CustomDivider } from "../styles/components/CustomDivider";
+import { EventPreviewBand } from "../components/EventPreviewBand";
+import { useEvents } from "../composables/useEvents";
 
 const Homepage: NextPage = () => {
   const { t } = useTranslation("homepage");
 
-  const latestEvent = {
-    id: "123",
-    name: "Rally du Picodon",
-    location: "Orange",
-    date: "12-12-2022",
-    coverImageUrl:
-      "https://project0-images.s3.eu-west-3.amazonaws.com/events/1638987409561_image",
-  };
-  const nextEvent = {
-    id: "123",
-    name: "Rally du Gard",
-    location: "Gard",
-    date: "23-12-2022",
-    coverImageUrl:
-      "https://project0-images.s3.eu-west-3.amazonaws.com/events/1639492173895_image",
-  };
+  const { isLoading, events, nextEvent } = useEvents();
 
   return (
     <AppBarWrapper>
       <PageLayout sx={{ rowGap: "20px" }}>
-        <Stack alignItems="center">
-          <Typography variant="h3">{t("latestEvent")}</Typography>
-          <Typography variant="h2">{latestEvent.name}</Typography>
-          <BandImage imageUrl={latestEvent.coverImageUrl} />
+        <Stack alignItems="center" sx={{ width: "100%" }}>
+          <Typography variant="h3">{t("homepage.latestEvent")}</Typography>
+          {!isLoading && events[0] && (
+            <EventPreviewBand
+              title={events[0].name}
+              imageUrl={events[0].coverImageUrl}
+              titleVariant="h2"
+            />
+          )}
         </Stack>
         <CustomDivider />
 
-        <Stack alignItems="center">
-          <Typography variant="h3">{t("nextEvent")}</Typography>
-          <Typography variant="h2">{nextEvent.name}</Typography>
-          <BandImage imageUrl={nextEvent.coverImageUrl} />
+        <Stack alignItems="center" sx={{ width: "100%" }}>
+          <Typography variant="h3">{t("homepage.nextEvent")}</Typography>
+          {!isLoading && nextEvent && (
+            <EventPreviewBand
+              title={nextEvent.name}
+              imageUrl={nextEvent.coverImageUrl}
+              titleVariant="h2"
+            />
+          )}
         </Stack>
         <CustomDivider />
       </PageLayout>
